30e586fc83963e06e14eb659494a0ce4d4a6188f
Bruno Griffone <brunogriffone15@gmail.com>
2024-12-26 20:29:11 -0300

    initial config and component tests

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PrimeNG } from 'primeng/config';
+import { ButtonModule } from 'primeng/button';
+import { ToggleSwitchModule } from 'primeng/toggleswitch';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ButtonModule, ToggleSwitchModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'primeng-app';
+export class AppComponent implements OnInit {
+  checked : boolean = false;
+  constructor(private primeng: PrimeNG) {}
+  ngOnInit(): void {
+    this.primeng.ripple.set(true);
+  }
 }
